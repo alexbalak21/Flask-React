b5b9a4857f0ae88f43207bcb8d85addd222e7448
Alex Balak <alex.balak@outlook.com>
2026-01-14 23:11:43 +0100

    fixed the favicon getting 404

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory   
 
 app = Flask(
     __name__,
@@ -9,3 +9,8 @@ app = Flask(
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/react.svg')
+def favicon():
+    return send_from_directory('templates', 'favicon.svg')
+
